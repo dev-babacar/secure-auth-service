f80139d1e785458ba9ede7aa2a0bdcd6aceeb1c7
Babacar Mane <babacar.mane.dev@gmail.com>
2026-08-10 19:59:21 +0000

    fix: add missing MockBeans for JwtDecoder and TokenBlacklist in WebMvcTest

--- src/test/java/com/babacar/secureauthservice/adapter/in/web/GlobalExceptionHandlerTest.java
+++ src/test/java/com/babacar/secureauthservice/adapter/in/web/GlobalExceptionHandlerTest.java
@@ -3,12 +3,14 @@ package com.babacar.secureauthservice.adapter.in.web;
 import com.babacar.secureauthservice.domain.port.in.LoginUseCase;
 import com.babacar.secureauthservice.domain.port.in.RefreshTokenUseCase;
 import com.babacar.secureauthservice.domain.port.in.RegisterUserUseCase;
+import com.babacar.secureauthservice.domain.port.out.TokenBlacklist;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,6 +34,12 @@ class GlobalExceptionHandlerTest {
 
     @MockBean
     private RefreshTokenUseCase refreshTokenUseCase;
+
+    @MockBean
+    private JwtDecoder jwtDecoder;
+
+    @MockBean
+    private TokenBlacklist tokenBlacklist;
 
     @Test
     @DisplayName("409 : email déjà utilisé")
